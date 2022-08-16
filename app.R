aa137fcb33d3ddcfaf402062ed0d3a8e17ede47c
Chris Hernandez <christopher.hernandez@unh.edu>
2022-08-16 15:49:40 -0400

    update

--- app.R
+++ app.R
@@ -1,13 +1,15 @@
 
 library(shiny)
 library(stringr)
+library(readxl)
+library(xlsx)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     titlePanel("Milling Data Formatter"),
     fluidRow(
         column(8,
-               fileInput("upload", "Upload a file", accept="csv"),
+               fileInput("upload", "Upload a file", accept="xlsx"),
                downloadButton("download")
         )
     )
@@ -19,8 +21,9 @@ server <- function(input, output) {
     
     data <- reactive({
         req(input$upload)
-        infile <- read.csv(input$upload$datapath, header=F)
+        infile <- as.data.frame(read_excel(input$upload$datapath, col_names=F))
         new <- c()
+	print(infile[,1])
         for(ent in infile[,1]){
             if(ent == "NO SEED"){
                 new <- c(new, c("NO SEED", "NO SEED"))
@@ -29,7 +32,8 @@ server <- function(input, output) {
             }
         }
         dataMat <- matrix(new, ncol=3, byrow=T)
-        colnames(dataMat) <- c("plot_number", "Total", "Whole")
+        colnames(dataMat) <- c("observationunit_name", "LSU01:0000101",
+			      "LSU01:0000100")
         dataMat[,2] <- str_replace_all(dataMat[,2], "[:alpha:]", "")
         dataMat[,3] <- str_replace_all(dataMat[,3], "[:alpha:]", "")
         dataMat[,2] <- str_replace_all(dataMat[,2], "\\?", "")
@@ -40,10 +44,10 @@ server <- function(input, output) {
     })
     output$download <- downloadHandler(
         filename = function(){
-            paste0("formatted_",input$upload$name)
+            paste0("formatted_",input$upload$name, ".xls")
         },
         content = function(file){
-            write.csv(data(), file)
+            write.xlsx(data(), file, row.names=F)
         }
     )
 }
